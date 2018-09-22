918856bb6fffa652694f792d93edec59ff617a76
mYahagi <y.masa7779@gmail.com>
2018-09-23 01:49:24 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+(function($){
+
+$(document).ready(function(){
+
+  var thumbs = $(".thumb img");
+  var top_images = $(".top img");
+
+  thumbs.hover(function(){
+    top_images.hide().eq(thumbs.index($(this))).show(); //一旦隠してから表示する
+  });
+
+});
+
+})(jQuery);
